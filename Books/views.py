5f5d2cfcc8610a34fa97bd73c7d7a4a4d30c2620
muhammad-saleh-hamidi <msaleh.hamidi@gmail.com>
2023-03-29 00:00:06 +0330

    add detail book

--- Books/views.py
+++ Books/views.py
@@ -7,3 +7,9 @@ class BookListView(generic.ListView):
     model = Book
     template_name = 'book/book_list.html'
     context_object_name = 'books'
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name = 'book/detail_book.html'
+
