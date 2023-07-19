f1b32fe6691525bb1eb34ad367983c2ac7aa83aa
muhammad-saleh-hamidi <msaleh.hamidi@gmail.com>
2023-07-19 14:05:49 +0330

    'booklist'

--- books/models.py
+++ books/models.py
@@ -4,5 +4,8 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
-    content = models.TextField()
+    description = models.TextField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.title
--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,8 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pages.urls')),
+    # path('', include('pages.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
+    path('',include('books.urls')),
 ]
